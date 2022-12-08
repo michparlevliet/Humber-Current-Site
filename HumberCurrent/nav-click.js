87790ffb8dde3e38a87e4b6d6815434a093acec7
Michelle Parlevliet <113198354+michparlevliet@users.noreply.github.com>
2022-12-08 15:28:36 -0500

    Humber Current Main v4
    To do still:
    - Replace dummy content with sample files
    - Add further CSS styling (drop-shadows, color, border-radius, some items needing flex/layout adjustments in the project section
    - Source and add icons

---
+++ HumberCurrent/nav-click.js
@@ -0,0 +1,10 @@
+// window.onload = function() {
+
+//   var mainNav = document.getElementById("main-menu");
+//   var secondNav = document.getElementById("dropdown");
+
+//   mainNav.onclick = function() {
+//     secondNav.classList.add("clicked");
+//   }
+
+// }
